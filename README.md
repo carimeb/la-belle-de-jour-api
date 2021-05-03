ba394bb04ecdbe1644ba2d64eecc0bab5a5e8162
cbumaruf <cbumaruf@pagseguro.com>
2021-05-03 17:31:32 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# La Belle Du Jour API
+
+## Sobre
+Esse é um agregador de preços de produtos de beleza.
+
+### Dependências
+- Spring Web
+- H2 database
+- Lombok
+- JPA
---
+++ src/test/java/com/labelledejour/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labelledejour.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
